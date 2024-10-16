aaf6c785263425b244719df46d0c05e3ab6b91fb
Hadrian Finch <hadrianfinch@outlook.com>
2024-10-16 17:23:23 +0100

    fix slide

--- src/main/java/com/skadoosh/wilderlands/mixin/LivingEntityMixin.java
+++ src/main/java/com/skadoosh/wilderlands/mixin/LivingEntityMixin.java
@@ -17,6 +17,7 @@ import com.skadoosh.wilderlands.persistance.DashComponent;
 import com.skadoosh.wilderlands.persistance.LiftComponent;
 import com.skadoosh.wilderlands.persistance.ModComponentKeys;
 
+import net.minecraft.block.Block;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.LivingEntity;
@@ -27,23 +28,21 @@ import net.minecraft.util.math.Vec3d;
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin
 {
-    // @Inject(method = "handleFrictionAndCalculateMovement", at = @At("HEAD"))
-    // @ModifyVariable(method = "handleFrictionAndCalculateMovement", at = @At("HEAD"), ordinal = 0, argsOnly = true)
-    // private float handleFrictionAndCalculateMovement(float slipperiness)
-    // {
-    //     LivingEntity e = ((LivingEntity)((Object)this));
-    //     int level = EnchantmentHelper.getHighestEquippedLevel(e.getRegistryManager().getLookupOrThrow(RegistryKeys.ENCHANTMENT).getHolderOrThrow(ModEnchantments.SLIDE), e);
-    //     float slip = ((level > 0) && e.isSneaking()) ? 1 : slipperiness;
+    @WrapOperation(method = "travel", at = @At(value="INVOKE", target = "Lnet/minecraft/block/Block;getSlipperiness()F"))
+    private float removeFriction(Block block, Operation<Float> origional)
+    {
+        LivingEntity e = ((LivingEntity)((Object)this));
+        int level = EnchantmentHelper.getHighestEquippedLevel(e.getRegistryManager().getLookupOrThrow(RegistryKeys.ENCHANTMENT).getHolderOrThrow(ModEnchantments.SLIDE), e);
 
-    //     return slip;
-    //     // origional.call(movementInput, ((level > 0) && e.isSneaking()) ? 1 : slipperiness);
-    // }
+        float f = origional.call(block);
+        return (e.isSneaking() && level > 0) ? 0.99999999999f : f;
+    }
 
     @Inject(method = "hasNoDrag", at = @At("HEAD"), cancellable = true)
     private void hasNoDrag(CallbackInfoReturnable<Boolean> ci)
     {
         LivingEntity e = ((LivingEntity)((Object)this));
-        if (e.isSneaking())
+        if (e.isSneaking() && e.isOnGround())
         {
             int level = EnchantmentHelper.getHighestEquippedLevel(e.getRegistryManager().getLookupOrThrow(RegistryKeys.ENCHANTMENT).getHolderOrThrow(ModEnchantments.SLIDE), e);
             if (level > 0)
@@ -53,33 +52,15 @@ public class LivingEntityMixin
                 return;
             }
         }
-        
-        // if (!e.isOnGround())
-        // {
-        //     int level = EnchantmentHelper.getHighestEquippedLevel(e.getRegistryManager().getLookupOrThrow(RegistryKeys.ENCHANTMENT).getHolderOrThrow(ModEnchantments.AERODYNAMIC), e);
-        //     if (level > 0)
-        //     {
-        //         ci.setReturnValue(true);
-        //         ci.cancel();
-        //         return;
-        //     }
-        // }
     }
 
     @WrapOperation(method = "handleFrictionAndCalculateMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getMovementSpeed(F)F"))
     private float injected(LivingEntity e, float slipperiness, Operation<Float> origional)
     {
-        // LivingEntity e = ((LivingEntity)((Object)this));
         RegistryLookup<Enchantment> lookupOrThrow = e.getRegistryManager().getLookupOrThrow(RegistryKeys.ENCHANTMENT);
         int adrenaline = EnchantmentHelper.getHighestEquippedLevel(lookupOrThrow.getHolderOrThrow(ModEnchantments.ADRENALINE), e);
-        // int slide = EnchantmentHelper.getHighestEquippedLevel(lookupOrThrow.getHolderOrThrow(ModEnchantments.SLIDE), e);
 
         float speed = origional.call(e, slipperiness);
-
-        // if (e.isSneaking() && (slide > 0))
-        // {
-        //     speed = e.getMovementSpeed();
-        // }
 
         if (adrenaline > 0)
         {
