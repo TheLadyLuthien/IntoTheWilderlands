4d07dbcd7fecd0a2359d5af288cae1426f9111eb
Hadrian Finch <hadrianfinch@outlook.com>
2024-11-22 22:05:48 +0000

    start work on remove_vanilli_enchants

---
+++ src/main/java/com/skadoosh/wilderlands/mixin/ServerPlayNetworkHandlerMixin.java
@@ -0,0 +1,23 @@
+package com.skadoosh.wilderlands.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ServerPlayNetworkHandler.class)
+public class ServerPlayNetworkHandlerMixin
+{
+	@ModifyReturnValue(method = "shouldCheckMovement", at = @At("RETURN"))
+	public boolean enchancement$disableVelocityChecksQuickly(boolean value)
+	{
+		return false;
+	}
+
+	@ModifyExpressionValue(method = "onPlayerMove", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;isInTeleportationState()Z"))
+	public boolean enchancement$disableVelocityChecksWrongly(boolean value)
+	{
+		return true;
+	}
+}
