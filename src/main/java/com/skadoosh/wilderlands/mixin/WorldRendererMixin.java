912b0ead2516c207e59ec83e9b76e16eef29c984
Hadrian Finch <hadrianfinch@outlook.com>
2024-11-30 19:57:33 +0000

    add veil

--- src/main/java/com/skadoosh/wilderlands/mixin/WorldRendererMixin.java
+++ src/main/java/com/skadoosh/wilderlands/mixin/WorldRendererMixin.java
@@ -24,18 +24,18 @@ public abstract class WorldRendererMixin
     @Shadow
     MinecraftClient client;
 
-    @Inject(
-        method = "renderSky",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;multiply(Lorg/joml/Matrix4f;)V", shift = At.Shift.AFTER),
-        locals = LocalCapture.CAPTURE_FAILHARD)
-    public void renderSky(Matrix4f projectionMatrix, Matrix4f matrix4f, float tickDelta, Camera preStep, boolean skipRendering, Runnable preRender, CallbackInfo ci, CameraSubmersionType cameraSubmersionType, MatrixStack matrixStack)
-    {
-        WorldRenderer thisRenderer = (WorldRenderer)((Object)this);
-        if (this.client.world.getSkyProperties() instanceof CustomSkyRenderer)
-        {
-            ((CustomSkyRenderer)this.client.world.getSkyProperties()).renderSky(thisRenderer, matrixStack, matrix4f, tickDelta);
-        }
-    }
+    // @Inject(
+    //     method = "renderSky",
+    //     at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;multiply(Lorg/joml/Matrix4f;)V", shift = At.Shift.AFTER),
+    //     locals = LocalCapture.CAPTURE_FAILHARD)
+    // public void renderSky(Matrix4f projectionMatrix, Matrix4f matrix4f, float tickDelta, Camera preStep, boolean skipRendering, Runnable preRender, CallbackInfo ci, CameraSubmersionType cameraSubmersionType, MatrixStack matrixStack)
+    // {
+    //     WorldRenderer thisRenderer = (WorldRenderer)((Object)this);
+    //     if (this.client.world.getSkyProperties() instanceof CustomSkyRenderer)
+    //     {
+    //         ((CustomSkyRenderer)this.client.world.getSkyProperties()).renderSky(thisRenderer, matrixStack, matrix4f, tickDelta);
+    //     }
+    // }
 
     @Accessor
     public abstract VertexBuffer getStarsBuffer();
